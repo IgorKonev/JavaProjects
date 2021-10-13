6f5d3f9eb47bc1d9ad9148b39756a2de2b42f01b
IgorKonev <90967076+IgorKonev@users.noreply.github.com>
2021-10-13 13:07:18 +0300

    Create LearnMain.java

---
+++ LearnMain.java
@@ -0,0 +1,15 @@
+package com.company;
+public class LearnMain {
+    public static void main(String[] args) {
+     //   System.out.println("Hello Ihar");
+
+        for (int i = args.length-1; i >= 0; i = i-1)
+        {
+
+            System.out.printf("rez-> %s",args[i]);
+        }
+
+      //   System.out.println(args[1]);
+     }
+    }
+
