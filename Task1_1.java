71ae76bb61c84dc42288b74d5d2735a17b63b74d
IgorKonev <90967076+IgorKonev@users.noreply.github.com>
2021-11-06 20:52:42 +0300

    Create Task1_1.java

---
+++ Task1_1.java
@@ -0,0 +1,28 @@
+/*найти значение функции */
+package module_1;
+
+import java.util.Scanner;
+
+public class Task1_1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Vvedite a : ");
+        double a = scan.nextDouble();
+
+        Scanner scan1 = new Scanner(System.in);
+        System.out.println("Vvedite b : ");
+        double b = scan1.nextDouble();
+
+        Scanner scan2 = new Scanner(System.in);
+        System.out.println("Vvedite c : ");
+        double c = scan2.nextDouble();
+
+    double z = ((a - 3) * b / 2) + c;
+
+    System.out.printf("rezult -> %.2f", z);
+
+    scan.close();
+    scan1.close();
+    scan2.close();
+    }
+}
